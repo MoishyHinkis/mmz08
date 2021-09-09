3dbcfa6a8810dff9f16cd61fab6c06feb8b9e164
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-09-09 16:45:17 +0300

    tweak: minor change

--- resources/js/Components/NewSizeForm.vue
+++ resources/js/Components/NewSizeForm.vue
@@ -1,44 +1,45 @@
 <template>
   <div>
-      <div class="w3-card w3-mobile center new-size">
-        <validation-errors></validation-errors>
-        <form @submit.prevent="sendForm" class="w3-container">
-          <h4 class="w3-center">Add New Size</h4>
-          <label for="name">name</label><br />
-          <input
-            type="text"
-            name="name"
-            v-model="form.name"
-            class="w3-input"
-            required
-          /><br /><br />
-          <label for="price"
-            >price in <i class="fas fa-shekel-sign"></i><br /> </label
-          ><br />
-          <input
-            type="number"
-            name="price"
-            v-model="form.price"
-            class="w3-input"
-            required
-          /> <br>
-          <input
-            type="file"
-            @input="filePath"
-            required
-            class="w3-input"
-          /><br /><br />
-          <button class="w3-btn w3-green w3-margin" type="submit">
-            Add Size
-          </button>
-        </form>
+    <div class="w3-card w3-mobile center new-size">
+      <validation-errors></validation-errors>
+      <form @submit.prevent="sendForm" class="w3-container">
+        <h4 class="w3-center">Add New Size</h4>
+        <label for="name">name</label><br />
+        <input
+          type="text"
+          name="name"
+          v-model="form.name"
+          class="w3-input"
+          required
+        /><br /><br />
+        <label for="price"
+          >price in <i class="fas fa-shekel-sign"></i><br /> </label
+        ><br />
+        <input
+          type="number"
+          name="price"
+          v-model="form.price"
+          class="w3-input"
+          required
+        />
+        <br />
+        <input
+          type="file"
+          @input="filePath"
+          required
+          class="w3-input"
+        /><br /><br />
+        <button class="w3-btn w3-green w3-margin" type="submit">
+          Add Size
+        </button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 import { useForm } from "@inertiajs/inertia-vue3";
-import ValidationErrors from ".././Components/ValidationErrors.vue";
+import ValidationErrors from "./ValidationErrors.vue";
 export default {
   name: "NewSizeForm",
   components: {
